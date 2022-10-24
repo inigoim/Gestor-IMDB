a8cf7285b95722df3854c597711869cf51919bc5
Iñigo <inigoim2@gmail.com>
2022-10-24 18:09:37 +0200

    Tarea b): ListaPeliculas, ListaInterpretes e Interprete (faltan los costes operacionales)

--- src/ListaPeliculas.java
+++ src/ListaPeliculas.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+
 public class ListaPeliculas {
+    // Lista interna
+    private ArrayList<Pelicula> lista = new ArrayList<>();
+
     /**
      * Añade una película a la lista
      * @param pel Película a añadir
      */
-    public void anadirPelicula(Pelicula pel) {}
+    public void anadirPelicula(Pelicula pel) {
+        lista.add(pel);
+    }
     /**
      * Busca una película en la lista y la devuelve
      * @param titulo Título de la película a buscar
      * @return la Película (si está en la lista), null en caso contrario
      */
-    public Pelicula buscarPelicula(String titulo) {}
+    public Pelicula buscarPelicula(String titulo) {
+        for (Pelicula pel: lista)
+            if (pel.getTitulo().equals(titulo)) return pel;
+        return null;
+    }
+
+    public ArrayList<Pelicula> getLista() {
+        return lista;
+    }
 }

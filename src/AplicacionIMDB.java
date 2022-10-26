3ab8a7eef2ad5eed60784d4584e702b64ecc2250
IvanCB <icalbo360@gmail.com>
2022-10-26 02:21:09 +0200

    Menu de AplicacionIMDB creado

--- src/AplicacionIMDB.java
+++ src/AplicacionIMDB.java
@@ -1,20 +1,21 @@
-package fase1;
-
 
 import java.util.Scanner;
 
 public class AplicacionIMDB {
-	
-	public static void main(String[] args){
 
+	public static CatalogoIMDB catalogo;
+	public static void main(String[] args){
+		catalogo = catalogo.getInstance();
           //TO DO: ...
 		
 	    //TO DO: Cargar películas	
+		catalogo.cargarPeliculas("films.txt");
 
 	    //TO DO Cargar intérpretes
-	
+		catalogo.cargarInterpretes("cast.txt");
 		
 		//Menú
+
 		Scanner sc = new Scanner(System.in);
 		int opcion=-1;
 		
@@ -27,10 +28,28 @@ public class AplicacionIMDB {
 			System.out.println("0. Salir");
 			opcion = Integer.parseInt(sc.nextLine());
 			switch(opcion) {
-			   case 1: 
-				    //TO DO
-			        break;
-	              // TO DO
+
+				case 1:
+					System.out.println("Introduce el nombre de una película: ");
+					String tituloPelicula = sc.nextLine();
+					catalogo.imprimirInfoPelicula(tituloPelicula);
+					break;
+
+				case 2:
+					System.out.println("Introduce el nombre de un interprete: ");
+					String nombreInterprete = sc.nextLine();
+					catalogo.imprimirInfoPelicula(nombreInterprete);
+					break;
+
+				case 3:
+					System.out.println("Introduce el nombre de la película que deseas puntuar: ");
+					tituloPelicula = sc.nextLine();
+					System.out.println("Introduce la punctuation que quieres darle a " + tituloPelicula + ": ");
+					float puntuacion = Float.parseFloat(sc.nextLine());
+					catalogo.anadirVoto(tituloPelicula, puntuacion);
+					break;
+
+				default:break;
 			}
 		}
 		sc.close();

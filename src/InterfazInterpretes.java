1320e26a5e26c3f66351f4542223e14f08a14da0
Iñigo <inigoim2@gmail.com>
2022-12-07 17:16:57 +0100

    Ivan, bro, lee los warnings

--- src/InterfazInterpretes.java
+++ src/InterfazInterpretes.java
@@ -1,14 +1,14 @@
 public interface InterfazInterpretes {
-    public void anadirInterprete(Interprete inter);
+    void anadirInterprete(Interprete inter);
 
-    public Interprete buscarInterprete(String nombre);
+    Interprete buscarInterprete(String nombre);
 
-    public Interprete eliminarInterprete(String nombre);
+    Interprete eliminarInterprete(String nombre);
 
     default boolean eliminarInterprete(Interprete inter) {
         if (inter == null) return false;
         return (eliminarInterprete(inter.getNombre()) != null);
     }
 
-    public int size();
+   int size();
 }

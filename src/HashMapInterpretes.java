9951d141fbd922d90c252ffe2d7b84ad11340600
IvanCB <icalbo360@gmail.com>
2023-01-09 00:29:47 +0100

    comentarios corregidos en la clase HashMapInterpretes

--- src/HashMapInterpretes.java
+++ src/HashMapInterpretes.java
@@ -4,14 +4,18 @@ public class HashMapInterpretes implements InterfazInterpretes {
     private final ConcurrentHashMap<String,Interprete> hmInterpretes = new ConcurrentHashMap<>();
 
 
-    /*
-     *Añade un intérprete al hashMap
+    /**
+     *Añade un intérprete al hashMap.
+     *@param inter Intérprete que se va a añadir.
      */
     public void anadirInterprete(Interprete inter) {
         hmInterpretes.put(inter.getNombre(),inter);
     }
-    /*
-     *Busca el intérprete en el hashMap
+    
+    /**
+     *Busca el intérprete en el hashMap.
+     * @param nombre nombre del intérprete que se quiere a buscar.
+     * @return Intérprete buscado o null en caso de no ser encontrado.
      */
     public Interprete buscarInterprete(String nombre) {
         if (hmInterpretes.containsKey(nombre))
@@ -19,16 +23,20 @@ public class HashMapInterpretes implements InterfazInterpretes {
         return null;
     }
 
-    /*
+    /**
      *Elimina el intérprete del hashMap, en caso de que el
      * intérprete no este devolverá null
+     * @param nombre nombre del intérprete a eliminar.
+     * @return Intérprete eliminado del Hashmap o null
+     * en caso de estar en el Hashmap.
      */
     public Interprete eliminarInterprete(String nombre) {
         return hmInterpretes.remove(nombre);
     }
 
-    /*
+    /**
      *Devuelve el número de elementos del hashMap
+     * @return Integer con el número de intérpretes del HashMap
      */
     public int size() {
         return hmInterpretes.size();

2902bccf13a118c1d85b48e12bc0287c5d3f67ea
IvanCB <icalbo360@gmail.com>
2023-01-09 18:47:39 +0100

    Orden temporal de los metodos de la clase HashMapInterpretes añadidos y metodo buscarinterpretes() corregido

--- src/HashMapInterpretes.java
+++ src/HashMapInterpretes.java
@@ -6,6 +6,7 @@ public class HashMapInterpretes implements InterfazInterpretes {
 
     /**
      *Añade un intérprete al hashMap.
+     *Es de orden constante, O(1).
      *@param inter Intérprete que se va a añadir.
      */
     public void anadirInterprete(Interprete inter) {
@@ -14,18 +15,18 @@ public class HashMapInterpretes implements InterfazInterpretes {
     
     /**
      *Busca el intérprete en el hashMap.
+     *Es de orden constante, O(1).
      * @param nombre nombre del intérprete que se quiere a buscar.
      * @return Intérprete buscado o null en caso de no ser encontrado.
      */
     public Interprete buscarInterprete(String nombre) {
-        if (hmInterpretes.containsKey(nombre))
-            return hmInterpretes.get(nombre);
-        return null;
+        return hmInterpretes.get(nombre);
     }
 
     /**
      *Elimina el intérprete del hashMap, en caso de que el
      * intérprete no este devolverá null
+     * Es de orden constante, O(1).
      * @param nombre nombre del intérprete a eliminar.
      * @return Intérprete eliminado del Hashmap o null
      * en caso de estar en el Hashmap.
@@ -36,6 +37,7 @@ public class HashMapInterpretes implements InterfazInterpretes {
 
     /**
      *Devuelve el número de elementos del hashMap
+     * Es de orden constante, O(1).
      * @return Integer con el número de intérpretes del HashMap
      */
     public int size() {
